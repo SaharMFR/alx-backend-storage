3ca9deb8e50118527809422b44a2b57be9c94e2f
SaharMFR <sweetysahora@gmail.com>
2024-06-13 23:24:47 +0000

    Update2 in task-5 0x00

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,8 +5,10 @@ DELIMITER $$
 CREATE TRIGGER validate_email
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF OLD.email != NEW.email THEN SET NEW.valid_email = 0;
-	ELSE SET NEW.valid_email = NEW.valid_email;
+	IF OLD.email != NEW.email THEN
+		SET NEW.valid_email = 0;
+	ELSE
+		SET NEW.valid_email = NEW.valid_email;
 	END IF;
 END $$
-DELIMITER
+DELIMITER ;
