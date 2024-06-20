87f7a26830d93f4545d80b64c363d22956a39de0
SaharMFR <sweetysahora@gmailcom>
2024-06-20 00:48:32 +0000

    Update2 in task-5 0x02

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,24 +6,23 @@ from typing import Callable
 from functools import wraps
 import requests
 
-
 redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
     """ Caches the output of fetched data """
     @wraps(method)
-    def invoker(url) -> str:
+    def wrapper(url):  # sourcery skip: use-named-expression
         """ The wrapper function for caching the output """
         redis_store.incr(f"count:{url}")
         res = redis_store.get(f"cached:{url}")
         if res:
             return res.decode("utf-8")
-        res = method(url)
-        redis_store.set(f"count:{url}", 0)
-        redis_store.setex(f"cached:{url}", 10, res)
-        return res
-    return invoker
+        html = method(url)
+        redis_store.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
 @data_cacher
