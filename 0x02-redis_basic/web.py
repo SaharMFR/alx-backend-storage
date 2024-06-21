618ca29e4a7ee40c710bda3f27a0a09b6cb8c788
SaharMFR <sweetysahora@gmailcom>
2024-06-21 08:00:36 +0000

    Update4 in task-5 0x02

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
 """ Requests caching and tracking """
 
-import redis
-from typing import Callable
 from functools import wraps
+import redis
 import requests
+from typing import Callable
 
-redis_store = redis.Redis()
+redis_ = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
+def count_requests(method: Callable) -> Callable:
     """ Caches the output of fetched data """
     @wraps(method)
     def wrapper(url):  # sourcery skip: use-named-expression
         """ The wrapper function for caching the output """
-        redis_store.incr(f"count:{url}")
-        res = redis_store.get(f"cached:{url}")
-        if res:
-            return res.decode('utf-8')
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
         html = method(url)
-        redis_store.setex(f"cached:{url}", 10, html)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
 
 
-@data_cacher
+@count_requests
 def get_page(url: str) -> str:
     """
     Returns the content of a URL after caching the request's response
     and tracking the request.
     """
-    request = requests.get(url)
-    return request.text
+    req = requests.get(url)
+    return req.text
